dd7b93aef316b544b66c433662a5611fead7bf17
UllSif <weckering.guillaume@live.fr>
2024-05-31 10:32:56 +0200

    Annonce data fetch improvement

--- src/pages/Annonces/index.jsx
+++ src/pages/Annonces/index.jsx
@@ -10,55 +10,56 @@ import Profile from "../../components/Profile";
 import Collapse from "../../components/Collapse";
 
 import "./style.scss";
+import {useEffect, useState} from "react";
 
 function Annonce() {
-    // const annonces = useFetch(window.location.origin + "/annonces.json");
-    // const {annonceId} = useParams();
-
-    let thisAnnonce;
-    // if (annonces.fetchedData) {
-    //     thisAnnonce = annonces.fetchedData.find(
-    //         (annonce) => annonce.id === annonceId
-    //     )
-    // }
+    const {annonceId} = useParams();
     const location = useLocation();
-    thisAnnonce = location.state;
+    const initialAnnonce = location.state || null;
+    const [thisAnnonce, setThisAnnonce] = useState(initialAnnonce);
 
-    // if (annonces.error) {
-    //     return <Error/>
-    // }
+    const { fetchedData, isLoading, error } = useFetch(thisAnnonce ? null : `${window.location.origin}/annonces.json`);
 
-    if (!thisAnnonce) {
-        return <Error/>
-    } else {
-        document.title = thisAnnonce.title + ' - Kasa';
-        return (
-            <section>
-                <Slideshow images={thisAnnonce.pictures}/>
-                <div className="annonce-info-rating-host-wrapper">
-                    <div className="annonce-info-wrapper">
-                        <h1 className="annonce-title">{thisAnnonce.title}</h1>
-                        <p className="annonce-location">
-                            {thisAnnonce.location}
-                        </p>
-                        <div className="tag-wrapper">
-                            {thisAnnonce.tags.map((tag, index) => (
-                                <Tag tagName={tag} key={`${tag}-${index}`} />
-                            ))}
-                        </div>
-                    </div>
-                    <div className="rating-host-wrapper">
-                        <Rating rating={thisAnnonce.rating} />
-                        <Profile name={thisAnnonce.host.name} picture={thisAnnonce.host.picture}/>
-                    </div>
-                </div>
-                <div className="description-equipments-wrapper">
-                    <Collapse title="Description" content={thisAnnonce.description} />
-                    <Collapse title="Équipements" content={thisAnnonce.equipments} />
-                </div>
-            </section>
-        )
+    useEffect(() => {
+        if (!thisAnnonce && fetchedData) {
+            const fetchedAnnonce = fetchedData.find((annonce) => annonce.id === annonceId);
+            setThisAnnonce(fetchedAnnonce);
+        }
+    }, [fetchedData, annonceId, thisAnnonce]);
+
+
+    if (error || isLoading || !thisAnnonce) {
+        return <Error />
     }
+
+    document.title = thisAnnonce.title + ' - Kasa';
+    return (
+        <section>
+            <Slideshow images={thisAnnonce.pictures}/>
+            <div className="annonce-info-rating-host-wrapper">
+                <div className="annonce-info-wrapper">
+                    <h1 className="annonce-title">{thisAnnonce.title}</h1>
+                    <p className="annonce-location">
+                        {thisAnnonce.location}
+                    </p>
+                    <div className="tag-wrapper">
+                        {thisAnnonce.tags.map((tag, index) => (
+                            <Tag tagName={tag} key={`${tag}-${index}`}/>
+                        ))}
+                    </div>
+                </div>
+                <div className="rating-host-wrapper">
+                    <Rating rating={thisAnnonce.rating}/>
+                    <Profile name={thisAnnonce.host.name} picture={thisAnnonce.host.picture}/>
+                </div>
+            </div>
+            <div className="description-equipments-wrapper">
+                <Collapse title="Description" content={thisAnnonce.description}/>
+                <Collapse title="Équipements" content={thisAnnonce.equipments}/>
+            </div>
+        </section>
+    )
 }
+
 
 export default Annonce;
